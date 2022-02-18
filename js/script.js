32deafa6e9c342958f7e6e686e8b74cfc3837ce3
unknown <mohasanimam@gmail.com>
2022-02-18 15:51:29 +0600

    error message updated

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
-const inputErrorMsg1 = 'please input numbers only';
-const inputErrorMsg2 = 'Input positive number only';
-
 function getInputInNmuber(inputId) {
     const input = document.getElementById(inputId);
     const inputInText = input.value;
     const inputAmount = parseFloat(inputInText);
-
-
     return inputAmount;
 }
 
 function getBalanceAfterDeduction(income, expense) {
     let balanceAfterDeduction = income - expense;
     return balanceAfterDeduction;
-    // error 1 expense cant be bigger than income***
 }
 
 // calculation of first event of income and expenses 
@@ -21,49 +15,35 @@ function getBalanceAfterDeduction(income, expense) {
 document.getElementById('calculate-button').addEventListener('click', function () {
     // getting amount of income
     const totalIncomeAmount = getInputInNmuber('income');
-
-    // getting amount of expenses 
-
-    // foods cost
-    const foodsCost = getInputInNmuber('foods-cost');
-
-    // foods cost
+    // getting sum of foods,rental and cloth costs
+    const foodsCost = getInputInNmuber('foods-cost')
     const rentalCost = getInputInNmuber('rent-cost');
-
-    // foods cost
     const clothsCost = getInputInNmuber('cloths-cost');
 
     if (totalIncomeAmount < 0 || foodsCost < 0 || rentalCost < 0 || clothsCost < 0) {
         alert("cannot input negative number");
         return;
-    } 
-    else if (isNaN(totalIncomeAmount) || isNaN(foodsCost) || isNaN(rentalCost) || isNaN(clothsCost )) {
-        alert("cannot input a string");
+    } else if (isNaN(totalIncomeAmount) || isNaN(foodsCost) || isNaN(rentalCost) || isNaN(clothsCost)) {
+        alert("Only numbers are Allowd");
         return;
     }
-
-
 
     // sum-up all expenses
     const totalExpenses = foodsCost + rentalCost + clothsCost;
 
-    // sum-up all expenses in total expense field 
+    // dedcting total expenses from total income using function
 
-    const totalExpenseField = document.getElementById('total-expenses');
-    totalExpenseField.innerText = totalExpenses;
-
-
-    // dedcting total expenses from total income and show in balance field
-    const totalBalance = getBalanceAfterDeduction(totalIncomeAmount, totalExpenses)
-    const balanceField = document.getElementById('balance');
-    balanceField.innerText = totalBalance;
-    
-    if (totalBalance < totalExpenses ) {
-        alert("you have not enough balance");
+    if (totalIncomeAmount < totalExpenses) {
+        alert("Income can't be less than your Expenses");
         return;
-    } 
+    } else {
+        const totalBalance = getBalanceAfterDeduction(totalIncomeAmount, totalExpenses)
+        const balanceField = document.getElementById('balance');
+        balanceField.innerText = totalBalance;
+        const totalExpenseField = document.getElementById('total-expenses');
+        totalExpenseField.innerText = totalExpenses;
+    }
 });
-
 
 // calculation of second event of savings
 
@@ -72,25 +52,30 @@ document.getElementById('save').addEventListener('click', function () {
 
     const totalIncomeAmount = getInputInNmuber('income');
 
-    const balanceField = document.getElementById('balance');
-    const totalBalanceAmount = balanceField.innerText;
-
-
     // getting amount of savings using percentage 
     const savingPercentage = getInputInNmuber('savings-percentage');
     const savingsAmount = (totalIncomeAmount * savingPercentage) / 100;
 
+
     const savingFieldAmount = document.getElementById('saving-amount');
     savingFieldAmount.innerText = savingsAmount;
 
+    const balanceField = document.getElementById('balance');
+    const totalBalanceAmount = balanceField.innerText;
+
+
     // calculating remaining balance
     const remainingBalanceField = document.getElementById('remaining-balance');
-
     const remainingBalanceAmount = totalBalanceAmount - savingsAmount;
-    remainingBalanceField.innerText = remainingBalanceAmount;
-
-    if (remainingBalanceAmount < savingsAmount ) {
-        alert("you have not enough balance");
+    
+    
+    if (totalBalanceAmount < savingsAmount) {
+        const remainingBalanceAmount = totalBalanceAmount - savingsAmount;
+        remainingBalanceField.innerText = remainingBalanceAmount;
+        alert("you have not enough balance For Savings");
         return;
     } 
+    else {
+        remainingBalanceField.innerText = remainingBalanceAmount;
+    }
 });
